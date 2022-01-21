7885437cd1a82b374429eebd70bd22101bdab8c2
aweaver <aweaver@student.42.fr>
2022-01-21 14:16:04 +0100

    updating gnl files

--- sources/get_next_line/get_next_line.c
+++ sources/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 16:11:32 by aweaver           #+#    #+#             */
-/*   Updated: 2022/01/13 13:49:08 by aweaver          ###   ########.fr       */
+/*   Updated: 2021/12/09 18:16:12 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,35 +25,25 @@ int	ft_strlen_custom(char *memory)
 {
 	int	i;
 
-	i = 1;
+	i = 0;
+	if (*memory == 0)
+		return (0);
 	while (memory[i] != 0 && memory[i] != '\n')
+		i++;
+	if (memory[i] == '\n')
 		i++;
 	return (i);
 }
-/* Working solution with leaks
+
 void	*ft_get_end(char *memory)
 {
+	if (*memory == 0)
+		return (memory);
 	while (*memory && *memory != '\n')
 		memory++;
 	if (*memory == '\n')
 		return (++memory);
 	return (memory);
-}
-*/
-
-void	*ft_get_end(char *memory)
-{
-	char	*temp;
-
-	while (*memory && *memory != '\n')
-		memory++;
-	if (*memory == '\n')
-		memory++;
-	temp = malloc(sizeof(*temp) + (ft_strlen(memory) + 1));
-	if (!temp)
-		return (0);
-	temp = ft_strjoin("", memory);
-	return (temp);
 }
 
 void	*ft_getline(char *memory)
@@ -62,42 +52,90 @@ void	*ft_getline(char *memory)
 	char	*line;
 
 	i = 0;
-	line = malloc(sizeof(*line) * ft_strlen_custom(memory) + 2);
+	line = malloc(sizeof(*line) * ft_strlen_custom(memory) + 1);
 	if (line == 0)
 		return (0);
-	while (memory[i] != '\n' && memory[i + 1] != '\0')
+	while (memory[i] != '\n' && memory[i] != '\0')
 	{
 		line[i] = memory[i];
 		i++;
 	}
 	line[i] = memory[i];
-	i++;
-	line[i] = 0;
+	if (line[i] == '\n')
+	{
+		i++;
+		line[i] = 0;
+	}
+	ft_strcpy(memory, ft_get_end(memory));
+	return (line);
+}
+
+char	*ft_make_magic(int fd, char *buffer, int bytes_read)
+{
+	static char	*memory;
+	char		*line;
+
+	if (memory == 0)
+	{
+		memory = malloc(sizeof(*memory) * (1));
+		if (memory == 0)
+			return (0);
+		memory[0] = 0;
+	}
+	while (bytes_read > 0 && ft_strchr(memory, '\n') == 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if ((bytes_read == -1 || (bytes_read == 0 && *memory == 0)))
+		{
+			free(buffer);
+			free(memory);
+			memory = NULL;
+			return (0);
+		}
+		buffer[bytes_read] = 0;
+		memory = ft_strjoin(memory, buffer);
+	}
+	line = ft_getline(memory);
 	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*memory = "";
-	char		*line;
 	char		*buffer;
+	char		*line;
 	int			bytes_read;
-	char		*temp;
 
-	bytes_read = 1;
-	buffer = malloc(sizeof(*buffer) *(BUFFER_SIZE + 1));
-	if (buffer == 0)
+	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0)
 		return (0);
-	while (bytes_read > 0 && ft_strchr(memory, '\n') == 0)
-	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		buffer[bytes_read] = 0;
-		memory = ft_strjoin(memory, buffer);
-	}
-	line = ft_getline(memory);
-	free(buffer);
-	temp = ft_get_end(memory);
-	ft_strcpy(memory, temp);
-	free(temp);
+	buffer = malloc(sizeof(buffer) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (0);
+	bytes_read = 1;
+	line = ft_make_magic(fd, buffer, bytes_read);
+	if (line != 0)
+		free(buffer);
 	return (line);
 }
+
+//int	main(void)
+//{
+	//int		i;
+	//int		fd;
+	//char	*str;
+//
+	//i = 0;
+	//fd = open("tata.txt", O_RDONLY);
+	//printf("%d\n", fd);
+	//if (fd == -1)
+	//{
+		//printf("le fichier n'a pas pu etre ouvert");
+		//return (0);
+	//}
+	//do{
+		//str = get_next_line(fd);
+		//printf("%s", str);
+		//free(str);
+		//i++;
+	//} while (str);
+	//return (0);
+//}
--- sources/get_next_line/get_next_line_utils.c
+++ sources/get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:39:08 by aweaver           #+#    #+#             */
-/*   Updated: 2021/12/08 11:44:58 by aweaver          ###   ########.fr       */
+/*   Updated: 2021/12/09 18:14:02 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ char	*ft_strcpy(char *dest, const char *str)
 	return (dest);
 }
 
-static char	*ft_strcat(char *dest, char *src)
+char	*ft_strcat(char *dest, char *src)
 {	
 	ft_strcpy(dest + ft_strlen(dest), src);
 	return (dest);
@@ -43,32 +43,33 @@ char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*str_joined;
 
-	if (!s1 || !s2)
-		return (0);
 	str_joined = malloc(sizeof(*str_joined) * (ft_strlen(s1) + ft_strlen(s2)
 				+ 1));
 	if (str_joined == 0)
 		return (0);
 	str_joined[0] = 0;
 	ft_strcat(str_joined, s1);
+	free(s1);
 	ft_strcat(str_joined, s2);
 	return (str_joined);
 }
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(char *s, int c)
 {
 	int	i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	if (c >= 256)
 		c %= 256;
 	while (s[i])
 	{
 		if (s[i] == c)
-			return ((char *)&s[i]);
+			return (&s[i]);
 		i++;
 	}
 	if (s[i] == c)
-		return ((char *)&s[i]);
+		return (&s[i]);
 	return (0);
 }
