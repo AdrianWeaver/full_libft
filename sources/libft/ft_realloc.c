de900b0d8a40b004dcd7cbf607182dc728372742
aweaver <aweaver@student.42.fr>
2022-02-18 12:31:00 +0100

    fixing mistake forgot to add ft_ prefix

--- sources/libft/ft_realloc.c
+++ sources/libft/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 11:34:57 by aweaver           #+#    #+#             */
-/*   Updated: 2022/02/18 12:08:24 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/02/18 12:30:26 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	*ft_realloc(void *ptr, size_t size)
 		return (NULL);
 	if (ptr != NULL)
 	{
-		memcpy(dest_ptr, ptr, size);
+		ft_memcpy(dest_ptr, ptr, size);
 		free(ptr);
 	}
 	return (dest_ptr);
