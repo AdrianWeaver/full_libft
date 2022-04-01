0f1f07f6b0bd4f9004dcc84d7acc2f144e7d35cc
aweaver <aweaver@student.42.fr>
2022-04-01 12:21:15 +0200

    FIX FT_ABS:
    Fix an issue where compiler would consider non void function could
    end without returning anything, by changing positive case to default.

--- sources/libft/ft_abs.c
+++ sources/libft/ft_abs.c
@@ -6,14 +6,13 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 10:32:55 by aweaver           #+#    #+#             */
-/*   Updated: 2022/04/01 10:33:53 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/04/01 12:20:03 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_abs(int nb)
 {
-	if (nb >= 0)
-		return (nb);
 	if (nb < 0)
 		return (-nb);
+	return (nb);
 }
