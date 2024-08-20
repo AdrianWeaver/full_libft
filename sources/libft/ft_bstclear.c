4f57d74c373b0a155bf63ad6334efe413f45e366
aweaver <aweaver@student.42.fr>
2024-08-20 09:39:43 +0200

    NORM BSTCLEAR:
    Fixing a small norm error.

--- sources/libft/ft_bstclear.c
+++ sources/libft/ft_bstclear.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 09:22:42 by aweaver           #+#    #+#             */
-/*   Updated: 2024/07/09 11:55:39 by aweaver          ###   ########.fr       */
+/*   Updated: 2024/08/20 09:37:15 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	ft_bstclear(t_bst **head, void (*del)(void *))
 {
 	ft_bstiterdepthfirst(head, del);
 	if (!head)
-		return;
+		return ;
 	*head = NULL;
 }
