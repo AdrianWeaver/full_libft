5fa7ab12275d3819e60f17e5ec3dc5940fe74633
aweaver <aweaver@student.42.fr>
2024-09-27 12:37:02 +0200

    FIX BST CLEAR:
    bstiter now clearing content instead of nodes as expected.

--- sources/libft/ft_bstiterdepthfirst.c
+++ sources/libft/ft_bstiterdepthfirst.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 10:38:26 by aweaver           #+#    #+#             */
-/*   Updated: 2024/08/20 14:05:52 by aweaver          ###   ########.fr       */
+/*   Updated: 2024/09/27 12:36:51 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_bstiterdepthfirst(t_bst **head, void (*func)(void *), int delete)
 		return ;
 	ft_bstiterdepthfirst(&(*head)->left, func, delete);
 	ft_bstiterdepthfirst(&(*head)->right, func, delete);
-	(*func)(*head);
+	(*func)((*head)->content);
 	if (delete == DELETENODES)
 	{
 		free(*head);
