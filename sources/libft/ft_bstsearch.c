eaf441cbbf83d21ca977f241c6316b79c03c000b
aweaver <aweaver@student.42.fr>
2024-09-28 16:08:07 +0200

    FIX FT_BSTSEARCH:
    Fixing bst search now optimized to look only one way.

--- sources/libft/ft_bstsearch.c
+++ sources/libft/ft_bstsearch.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 08:35:12 by aweaver           #+#    #+#             */
-/*   Updated: 2024/09/28 13:25:03 by aweaver          ###   ########.fr       */
+/*   Updated: 2024/09/28 16:07:17 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,19 @@
 
 t_bst	*ft_bstsearch(t_bst *tree, void *data, int (*cmp)(void *, void *))
 {
+	int cmp_result = 0;
+	t_bst *found = NULL;
 	if (!tree || !cmp)
 		return (NULL);
-	if ((*cmp)(tree->content, data) == 0)
+	cmp_result = (*cmp)(tree->content, data);
+	if (cmp_result == 0)
 		return (tree);
-	if (tree->left)
-		return (ft_bstsearch(tree->left, data, cmp));
-	if (tree->right)
-		return (ft_bstsearch(tree->right, data, cmp));
-	return (NULL);
+	if (cmp_result < 0)
+	{
+		found = ft_bstsearch(tree->left, data, cmp);
+		if (found)
+			return (found);
+	}
+	found = ft_bstsearch(tree->right, data, cmp);
+	return (found);
 }
